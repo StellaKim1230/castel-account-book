b0eed373f5204033c47f930ce9c196c9e89325cd
StellaKim1230 <stellakim1230@gmail.com>
2023-12-14 18:16:49 +0900

    feat: connected api and output response.

--- apps/web/app/routes/_index.tsx
+++ apps/web/app/routes/_index.tsx
@@ -1,41 +1,26 @@
-import type { MetaFunction } from "@remix-run/node";
+import { json, type MetaFunction } from '@remix-run/node';
+import { useLoaderData } from '@remix-run/react';
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: 'Castel Account Book' },
+    { name: 'description', content: 'Account Book of Lucas and Stella.' },
   ];
 };
 
+// Server-side rendering in Remix
+export async function loader() {
+  const res = await fetch('http://localhost:4000/');
+  const data = await res.text();
+  return json(data);
+}
+
 export default function Index() {
+  const text = useLoaderData<typeof loader>();
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}>
+      <h1>{text}</h1>
     </div>
   );
 }
