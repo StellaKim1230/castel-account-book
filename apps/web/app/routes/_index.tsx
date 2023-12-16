ce700bba6c5709446cfeca8f04d9054e75d4b7e6
StellaKim1230 <stellakim1230@gmail.com>
2023-12-16 17:39:12 +0900

    feat: setting dockerfile and start.

--- apps/web/app/routes/_index.tsx
+++ apps/web/app/routes/_index.tsx
@@ -10,17 +10,17 @@ export const meta: MetaFunction = () => {
 
 // Server-side rendering in Remix
 export async function loader() {
-  const res = await fetch('http://localhost:4000/');
-  const data = await res.text();
+  const res = await fetch('http://localhost:4000/users');
+  const data = await res.json();
   return json(data);
 }
 
 export default function Index() {
-  const text = useLoaderData<typeof loader>();
-
+  const data = useLoaderData<typeof loader>();
+  console.log('data', data);
   return (
     <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}>
-      <h1>{text}</h1>
+      <h1>Hello world!</h1>
     </div>
   );
 }
